2c60d4baeb19945f3aa6922e74c7af3f9267eb9d
dac <dac@dypm.org>
2023-07-26 16:08:40 +0530

    DotNet MovieMAnia Project Practice

---
+++ MovieMania/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieMania.Models
+{
+    [Index(nameof(MovieName), IsUnique =true)]
+    public class Movie
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Name of movie is required")]
+        public string MovieName { get; set; }
+        [Required(ErrorMessage ="Producer should be present")]
+        public string Producer { get; set;}
+
+        public double Ticket { get; set;}   
+
+        public DateOnly ReleaseDate { get; set;}    
+    }
+}
